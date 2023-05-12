3d8654734804f3f0bf232f6c9f2d2723e9dab375
Mohammad-Safari <msafari8051@gmail.com>
2023-05-12 12:01:04 +0330

    [add]max pooling 2d layer implemntation

--- layers/maxpooling2d.py
+++ layers/maxpooling2d.py
@@ -9,6 +9,9 @@ class MaxPool2D:
                 stride: stride of the kernel
                 mode: max or average
             Question:Why we don't need to set name for the layer?
+            Answer:The max pooling operation is performed using a pre-implemented layer,
+                typically called "MaxPooling2D" or something similar. so it's not necessary to
+                name the layer, and it does not have any efect on model correct functionality.
         """
         self.stride = (stride, stride) if isinstance(stride, int) else stride
         self.kernel_size = (kernel_size, kernel_size) if isinstance(kernel_size, int) else kernel_size
@@ -22,10 +25,13 @@ class MaxPool2D:
             returns:
                 output_shape: shape of the output
         """
-        # TODO: Implement target_shape
-        H = None
-        W = None
-        return H, W
+        # DONE: Implement target_shape
+        (batch_size, H_prev, W_prev, C_prev) = input_shape
+        (f_h, f_w) = self.kernel_size
+        strideh, stridew = self.stride
+        H = int(1 + (H_prev - f_h) / strideh)
+        W = int(1 + (W_prev - f_w) / stridew)
+        return (batch_size, H, W, C_prev)
     
     def forward(self, A_prev):
         """
@@ -35,25 +41,26 @@ class MaxPool2D:
             returns:
                 A: output of the max pooling layer
         """
-        # TODO: Implement forward pass for max pooling layer
-        (batch_size, H_prev, W_prev, C_prev) = None
-        (f_h, f_w) = None
-        strideh, stridew = None
-        H, W = None
-        A = np.zeros((None, None, None, None))
-        for i in range(None):
-            for h in range(None):
-                h_start = None
-                h_end = h_start + None
-                for w in range(None):
-                    w_start = None
-                    w_end = w_start + None
-                    for c in range(None):
+        # Implement forward pass for max pooling layer
+        (batch_size, H_prev, W_prev, C_prev) = A_prev.shape
+        (f_h, f_w) = self.kernel_size
+        strideh, stridew = self.stride
+        H = int(1 + (H_prev - f_h) / strideh)
+        W = int(1 + (W_prev - f_w) / stridew)
+        A = np.zeros((batch_size, H, W, C_prev))
+        for i in range(batch_size):
+            for h in range(H):
+                h_start = h * strideh
+                h_end = h_start + f_h
+                for w in range(W):
+                    w_start = w * stridew
+                    w_end = w_start + f_w
+                    for c in range(C_prev):
                         a_prev_slice = A_prev[i, h_start:h_end, w_start:w_end, c]
                         if self.mode == "max":
-                            A[i, h, w, c] = None
+                            A[i, h, w, c] = np.max(a_prev_slice)
                         elif self.mode == "average":
-                            A[i, h, w, c] = None
+                            A[i, h, w, c] = np.mean(a_prev_slice)
                         else:
                             raise ValueError("Invalid mode")
 
@@ -67,8 +74,8 @@ class MaxPool2D:
             returns:
                 mask: numpy array of the same shape as window, contains a True at the position corresponding to the max entry of x.
         """
-        # TODO: Implement create_mask_from_window
-        mask = x == None
+        # DONE:Implement create_mask_from_window
+        mask = x == np.max(x)
         return mask
     
     def distribute_value(self, dz, shape):
@@ -80,13 +87,13 @@ class MaxPool2D:
             returns:
                 a: distributed value
         """
-        # TODO: Implement distribute_value
+        # DONE:Implement distribute_value
         (n_H, n_W) = shape
-        average = None
-        a = np.ones(shape) * None
+        average = dz / (n_H * n_W)
+        a = np.ones(shape) * average
         return a
     
-    def backward(self, dZ, A_prev):
+    def backward(self, dA, A_prev):
         """
         Backward pass for max pooling layer.
             args:
@@ -95,30 +102,34 @@ class MaxPool2D:
             returns:
                 dA_prev: gradient of cost with respect to the input of the max pooling layer
         """
-        # TODO: Implement backward pass for max pooling layer
+        # DONE: Implement backward pass for max pooling layer
+        (batch_size, H, W, C) = dA.shape
         (f_h, f_w) = self.kernel_size
         strideh, stridew = self.stride
-        batch_size, H_prev, W_prev, C_prev = None
-        batch_size, H, W, C = None
-        dA_prev = np.zeros((None, None, None, None))
-        for i in range(None):
-            for h in range(None):
-                for w in range(None):
-                    for c in range(None):
-                        h_start = None
-                        h_end = h_start + None
-                        w_start = None
-                        w_end = w_start + None
-                        if self.mode == "max":
-                            a_prev_slice = A_prev[i, h_start:h_end, w_start:w_end, c]
-                            mask = self.create_mask_from_window(None)
-                            dA_prev[i, h_start:h_end, w_start:w_end, c] += np.multiply(None, None)
-                        elif self.mode == "average":
-                            dz = dZ[i, h, w, c]
-                            dA_prev[i, h_start:h_end, w_start:w_end, c] += self.distribute_value(None, None)
-                        else:
-                            raise ValueError("Invalid mode")
-        # Don't change the return
+        batch_size, H_prev, W_prev, C_prev = A_prev.shape
+        dA_prev = np.zeros((batch_size, H_prev, W_prev, C_prev))
+        for i in range(batch_size):
+            for h in range(H):
+                for w in range(W):
+                    for c in range(C):
+                        h_start = h
+                        h_end = h_start + f_h
+                    w_start = w * stridew
+                    w_end = w_start + f_w
+                    
+                    if self.mode == "max":
+                        a_prev_slice = A_prev[i, h_start:h_end, w_start:w_end, c]
+                        mask = self.create_mask_from_window(a_prev_slice)
+                        dA_prev[i, h_start:h_end, w_start:w_end, c] += np.multiply(mask, dA[i, h, w, c])
+                        
+                    elif self.mode == "average":
+                        shape = (f_h, f_w)
+                        dz = dA[i, h, w, c]
+                        dA_prev[i, h_start:h_end, w_start:w_end, c] += self.distribute_value(dz, shape)
+                        
+                    else:
+                        raise ValueError("Invalid mode")
+
         return dA_prev, None
     
     
